d3c7311abc2e8550ce73c4c37edbe6951c28fab5
woo-jk <wjk6044@gmail.com>
2025-01-15 15:16:46 +0900

    login

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import useForm from '../../hooks/useForm';
+import {validateLogin} from '../../utils';
+import InputField from '../../\bcomponents/InputField';
+import CustomButton from '../../\bcomponents/CustomButton';
 
 function LoginScreen() {
+  const login = useForm({
+    initialValue: {email: '', password: ''},
+    validate: validateLogin,
+  });
+
+  const handleSubmit = () => {
+    console.log('login.values', login.values);
+  };
+
   return (
-    <View>
-      <Text>로그인 화면</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error={login.errors.email}
+          touched={login.touched.email}
+          inputMode="email"
+          {...login.getTextInputProps('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error={login.errors.password}
+          touched={login.touched.password}
+          secureTextEntry
+          {...login.getTextInputProps('password')}
+        />
+      </View>
+      <CustomButton
+        label="로그인"
+        variant="filled"
+        size="large"
+        onPress={handleSubmit}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
 
 export default LoginScreen;
